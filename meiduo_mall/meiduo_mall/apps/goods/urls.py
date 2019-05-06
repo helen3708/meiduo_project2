5e68bee82a1cb8f59c7468869609cf59040ff2d3
helen3708 <1211284505@qq.com>
2019-05-07 01:08:25 +0800

    Day09-practice2

--- meiduo_mall/meiduo_mall/apps/goods/urls.py
+++ meiduo_mall/meiduo_mall/apps/goods/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import url, include
 from django.contrib import admin
 
-from .views import ListView,HotGoodsView
+from .views import ListView,HotGoodsView,DtailView
 
 urlpatterns = [
     url(r'^list/(?P<category_id>\d+)/(?P<page_num>\d+)/$', ListView.as_view()),
-    url(r'^hot/(?P<category_id>\d+)/',HotGoodsView.as_view()),
+    url(r'^hot/(?P<category_id>\d+)/$',HotGoodsView.as_view()),
+    url(r'^detail/(?P<sku_id>\d+)/$',DtailView.as_view()),
 ]
--- meiduo_mall/meiduo_mall/settings/dev.py
+++ meiduo_mall/meiduo_mall/settings/dev.py
@@ -44,6 +44,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    'haystack',# 全文检索
+
     'users',
     'oauth',
     'meiduo_mall.apps.areas',
@@ -255,3 +257,17 @@ EMAIL_VERIFY_URL = 'http://www.meiduo.site:8000/emails/verification/'
 # 修改Django的文件存储类
 DEFAULT_FILE_STORAGE = 'meiduo_mall.utils.fastdfs.fdfs_storage.FastDFSStorage'
 FDFS_BASE_URL = 'http://192.168.159.137:8888/'  # FastDFS中sotrage(nginx) ip和端口
+
+# Haystack
+HAYSTACK_CONNECTIONS = {
+    'default': {
+        'ENGINE': 'haystack.backends.elasticsearch_backend.ElasticsearchSearchEngine',
+        'URL': 'http://192.168.159.137:9200/', # Elasticsearch服务器ip地址，端口号固定为9200
+        'INDEX_NAME': 'meiduo_mall2', # Elasticsearch建立的索引库的名称
+    },
+}
+
+# 当添加、修改、删除数据时，自动生成索引
+HAYSTACK_SIGNAL_PROCESSOR = 'haystack.signals.RealtimeSignalProcessor'
+
+HAYSTACK_SEARCH_RESULTS_PER_PAGE = 5  # 搜索出来的数据每页显示多少条
